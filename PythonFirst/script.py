991ca24829140cf6073a74cf7492093cfec899d7
Matthew <realazurenova@gmail.com>
2026-01-30 23:10:54 +0100

    Dictionary test

--- PythonFirst/script.py
+++ PythonFirst/script.py
@@ -3,3 +3,15 @@ if 5>4:
     print("TRUTH NUKE")
 var = 55
 print("Variable", var)
+
+tempDict = {
+    "String": "YUP",
+    "Number": 123,
+    "Bool": True,
+    "Dict":{
+        "Key1": 1,
+        "Key2": 2,
+    },
+    "List": ["Yes", "Maybe", "No"]
+}
+print(f"- {tempDict['String']}\n- {tempDict['Number']}\n- {tempDict['Bool']}\n- {tempDict['Dict']['Key2']}\n- {tempDict['List'][1]}")
