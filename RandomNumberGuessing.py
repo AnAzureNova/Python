2467f9a0c494aafd656d896b0b8279559bb28141
Matěj Holeček <st025840@student.cichnovabrno.cz>
2026-02-10 09:34:11 +0100

    Random numbers

---
+++ RandomNumberGuessing.py
@@ -0,0 +1,22 @@
+import random
+
+atts = 1
+fromRange = 1
+toRange = 15
+print(f"Guess the number ({fromRange}-{toRange})")
+rngnum = random.randint(fromRange, toRange)
+
+def intinput():
+    while True:
+        try:
+            return int(input(f"[{atts}] > "))
+        except ValueError:
+            print("Invalid input")
+
+while True:
+    guess = intinput()
+    if guess == rngnum:
+        print(f"You guessed the number in {atts} attempts")
+        exit(0)
+    else:
+        atts += 1
